f5c9ff82662ee83c8f46a9c29736e5ec96324bf2
root <root@ip-172-31-23-254.eu-central-1.compute.internal>
2015-04-08 18:14:01 -0400

    it movesgit add huinya_master.py

--- huinya_master.py
+++ huinya_master.py
@@ -2,18 +2,16 @@
 
 import sys
 import os
-#from tornado import ioloop
-#import tornado.web
+from tornado import ioloop
+import tornado.web
 from huinya_master_settings import *
 
 
-#class UploadHandler(tornado.web.RequestHandler):
-#    def get(self):
-#        print "Hello, world!"
+class UploadHandler(tornado.web.RequestHandler):
+    def get(self):
+        print "Hello, world!"
 
 if __name__ == "__main__":
-    print os.environ['PATH']
     application = tornado.web.Application([(r"/upload", UploadHandler)])
     application.listen(PORT)
-    tornado.ioloop.instance().start()
-
+    tornado.ioloop.IOLoop.instance().start()
